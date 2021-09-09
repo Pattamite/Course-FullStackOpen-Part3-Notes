de0620884d1137805d989e8276d596ec98c4baf0
pattamite <jittapat007@gmail.com>
2021-09-09 17:20:31 +0700

    Add note validator

--- index.js
+++ index.js
@@ -3,27 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const Note = require("./models/notes");
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 const app = express();
 
 app.use(cors());
@@ -68,37 +47,26 @@ app.put("/api/notes/:id", (request, response) => {
 
 app.delete("/api/notes/:id", (request, response) => {
   Note.findByIdAndRemove(request.params.id)
-  .then((result) => {
-    response.status(204).end();
-  })
-  .catch((error) => {next(error);});
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {next(error);});
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+
+  note.save()
+    .then((savedNote) => {
+      response.json(savedNote.toJSON());
+    })
+    .catch((error) => {next(error);});
 });
-
-function generateId() {
-  //  Math.max can't receive whole array,
-  //    so we need to use ... to transform array to individual number
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-
-  return maxId + 1;
-}
 
 function unknownEndpoint(request, response) {
   response.status(404).send({error: 'unknown endpoint'});
@@ -110,6 +78,8 @@ function errorHandler(error, request, response, next) {
 
   if(error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'});
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message});
   }
 
   next(error);
